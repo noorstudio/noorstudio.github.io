0e29c1d5df2c12c2681105ea2f7bf5d119b38d7f
piotr-golawski <piotrgolawski@hotmail.com>
2019-05-06 16:38:47 +0100

    New version

---
+++ precache-manifest.6245569eed5f5582508fd0735611d7bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "019b1676693046b8696e6911b19e6b2b",
+    "url": "/static/media/teacup-sq.019b1676.jpg"
+  },
+  {
+    "revision": "e922a0ae297b0fd8ab89",
+    "url": "/static/css/main.f4538fb8.chunk.css"
+  },
+  {
+    "revision": "7f51830ad80cb21bb797",
+    "url": "/static/js/1.7f51830a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfb7bfca61e7d64fbad9ec6f922e9978",
+    "url": "/static/media/shelf-sq.bfb7bfca.jpg"
+  },
+  {
+    "revision": "54a8aba9964ececc4a12207810e0a7a5",
+    "url": "/static/media/oils-sq.54a8aba9.jpg"
+  },
+  {
+    "revision": "e922a0ae297b0fd8ab89",
+    "url": "/static/js/main.e922a0ae.chunk.js"
+  },
+  {
+    "revision": "ae8c09cfb88df3d02031d2379bfa8446",
+    "url": "/static/media/towels-sq.ae8c09cf.jpg"
+  },
+  {
+    "revision": "b246cac73779c2cf71de563a3b625603",
+    "url": "/static/media/shelfside-sq.b246cac7.jpg"
+  },
+  {
+    "revision": "89ebb02e133833a89deb1c46f1906291",
+    "url": "/static/media/oils2-sq.89ebb02e.jpg"
+  },
+  {
+    "revision": "0f304f81f7a74dbfcff158c9b38919ec",
+    "url": "/static/media/oils2-opacity.0f304f81.png"
+  },
+  {
+    "revision": "8c90c810fe2f0b33af08deb4e4c1c80d",
+    "url": "/index.html"
+  }
+];
